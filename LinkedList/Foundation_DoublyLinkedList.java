ca45ac4d238a0ce40d94af9ff65e352f5872a45b
shivrya <shivrajkhose4@gmail.com>
2025-05-20 22:17:09 -0700

    basics of doubly linked list

--- LinkedList/Foundation_DoublyLinkedList.java
+++ LinkedList/Foundation_DoublyLinkedList.java
@@ -1,36 +1,57 @@
 package Data_Structures_Algorithms.LinkedList;
 
-
-class Node{
+class ListNode {
     int value;
-    Node prev;
-    Node next;
+    ListNode prev;
+    ListNode next;
 
-    Node(){
+    ListNode() {
         this.value = 0;
         this.prev = null;
         this.next = null;
     }
 
-    Node(int value,Node prev, Node next){
+    ListNode(int value, ListNode prev, ListNode next) {
         this.value = value;
         this.prev = prev;
         this.next = next;
     }
 
-    Node(int value){
+    ListNode(int value) {
         this.value = value;
-        this.next = null;
         this.prev = null;
+        this.next = null;
     }
-
 }
-
 
 public class Foundation_DoublyLinkedList {
 
     public static void main(String[] args) {
-        
+        int[] nums = {10, 20, 30, 40};
+        ListNode head = arrayToLinkedList(nums);
+        printList(head);
     }
 
+    public static ListNode arrayToLinkedList(int[] nums) {
+        if (nums.length == 0) return null;
+
+        ListNode head = new ListNode(nums[0], null, null);
+        ListNode tmp = head;
+
+        for (int i = 1; i < nums.length; i++) {
+            ListNode newNode = new ListNode(nums[i], tmp, null);
+            tmp.next = newNode;
+            tmp = newNode;
+        }
+        return head;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode tmp = head;
+        while (tmp != null) {
+            System.out.print(tmp.value + " ");
+            tmp = tmp.next;
+        }
+        System.out.println();
+    }
 }
