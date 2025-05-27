0b32fda99a10710d48b7e7e3bee806cc424e8f1d
shivrya <shivrajkhose4@gmail.com>
2025-05-27 03:13:48 -0700

    Patters: Pattern 1

---
+++ patterns/Pattern1.java
@@ -0,0 +1,29 @@
+package Data_Structures_Algorithms.patterns;
+
+public class Pattern1 {
+    /* 
+       *****
+       *****
+       *****
+       *****
+       *****  
+    */
+
+
+    public static void main(String[] args) {
+        int N = 5;
+
+        pattern1(N);
+    }
+
+    public static void pattern1(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
