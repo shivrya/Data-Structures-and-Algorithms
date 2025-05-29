4924b51fbf6ec201393ed91a6894f7135f72533e
shivrya <shivrajkhose4@gmail.com>
2025-05-28 23:34:16 -0700

    Greedy Algorithm: Shortest Job First Main method

--- Greedy_Algorithm/Scheduling_and_Interval_Problems/ShortestJobFirst.java
+++ Greedy_Algorithm/Scheduling_and_Interval_Problems/ShortestJobFirst.java
@@ -4,7 +4,20 @@ import java.util.Arrays;
 
 public class ShortestJobFirst {
 
-    public long solve(int[] bt) {
+    public static void main(String[] args) {
+        int[] jobs = {1, 2, 3, 4};
+
+        System.out.print("Array Representing Job Durations: ");
+        for (int job : jobs) {
+            System.out.print(job + " ");
+        }
+        System.out.println();
+
+        long ans = solve(jobs);
+        System.out.println("Total waiting time: " + ans);
+    }
+
+    public static long solve(int[] bt) {
         Arrays.sort(bt);
         long waitTime = 0;
         int processes = bt.length -1;
