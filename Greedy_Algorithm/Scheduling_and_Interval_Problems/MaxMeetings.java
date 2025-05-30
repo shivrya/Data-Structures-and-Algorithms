69b22fa995a81c7254d4fd13e424a37b62291009
shivrya <shivrajkhose4@gmail.com>
2025-05-29 21:09:09 -0700

    Greedy Algorithm: Maximum Meetings

--- Greedy_Algorithm/Scheduling_and_Interval_Problems/MaxMeetings.java
+++ Greedy_Algorithm/Scheduling_and_Interval_Problems/MaxMeetings.java
@@ -3,8 +3,17 @@ package Data_Structures_Algorithms.Greedy_Algorithm.Scheduling_and_Interval_Prob
 import java.util.Arrays;
 
 public class MaxMeetings {
+        public static void main(String[] args) {
+            // Start and end times of the meetings
+            int[] start = {1, 3, 0, 5, 8, 5};
+            int[] end = {2, 4, 6, 7, 9, 9};
+            // Get the maximum number of meetings that can be held
+            int maxMeetings = maxMeetings(start, end);
+            // Output the maximum number of meetings
+            System.out.println("Maximum number of meetings: " + maxMeetings);
+        }
 
-        public int maxMeetings(int[] start, int[] end) {
+        public static int maxMeetings(int[] start, int[] end) {
            //your code goes here
            int[][] combined = new int[start.length][2];
     
