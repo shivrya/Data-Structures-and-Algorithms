806253b022b4179f162ae619add66a33f4b9fdbf
shivrya <shivrajkhose4@gmail.com>
2025-05-31 23:14:22 -0700

    Longest non repeating substring: Approach 1 using brute force

---
+++ SlidingWindow_TwoPointer/LongestNonRepeatingSubstring.java
@@ -0,0 +1,38 @@
+package Data_Structures_Algorithms.SlidingWindow_TwoPointer;
+
+import java.util.HashSet;
+
+public class LongestNonRepeatingSubstring {
+
+    public static void main(String[] args) {
+        String input = "cadbzabcd";
+        
+        int length = longestNonRepeatingSubstring(input);
+        
+        // Print the result
+        System.out.println("Length of longest substring without repeating characters: " + length);
+    }
+
+    public static int longestNonRepeatingSubstring(String s) {
+        int n = s.length();
+        
+        int maxlength = 0;
+
+        // APPROACH 1:
+        for(int i=0;i<n;i++){
+            HashSet<Character> set = new HashSet<>();
+            set.add(s.charAt(i));
+       
+            for(int j=i+1;j<n;j++){
+                if(set.contains(s.charAt(j))){
+                    break;
+                } 
+                set.add(s.charAt(j));
+                maxlength = Math.max(maxlength,j-i+1);
+            }
+        }
+        return maxlength;
+        
+
+    }
+}
