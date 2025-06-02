76f938be0050b15145bd3d5dc2a5dbf0a80fac1a
shivrya <shivrajkhose4@gmail.com>
2025-06-01 23:22:32 -0700

    Pattern 2

---
+++ patterns/Pattern2.java
@@ -0,0 +1,25 @@
+package Data_Structures_Algorithms.Patterns;
+
+public class Pattern2 {
+
+    public static void main(String[] args) {
+        int N = 5;
+        pattern2(N);
+    }
+
+    public static void pattern2(int n) {
+
+        for(int i=1;i<=n;i++){
+            
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
