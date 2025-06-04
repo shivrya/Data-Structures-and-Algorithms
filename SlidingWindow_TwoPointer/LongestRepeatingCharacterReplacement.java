f35c6b4a0ad7902bca1d4a888a58cb9b1f988c39
shivrya <shivrajkhose4@gmail.com>
2025-06-03 18:48:55 -0700

    Longest Repeating Character Replacement

---
+++ SlidingWindow_TwoPointer/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,25 @@
+package Data_Structures_Algorithms.SlidingWindow_TwoPointer;
+
+public class LongestRepeatingCharacterReplacement {
+
+    public static void main(String[] args) {
+        String s = "AABABBA";
+        int k = 2;
+
+        int length = characterReplacement(s, k);
+
+        // Print the result
+        System.out.println("Maximum length of substring with at most " + k + " characters replaced: " + length);
+    }
+    
+    public static int characterReplacement(String s, int k) {
+    
+            int maxlength = 0;
+
+
+            return maxlength;
+
+
+            
+        }
+}
