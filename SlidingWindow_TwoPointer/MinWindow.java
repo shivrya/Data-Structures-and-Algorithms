be06190b7b2e0f8a088e73b451a15567ce619aa8
shivrya <shivrajkhose4@gmail.com>
2025-06-04 19:15:49 -0700

    Minimum Window Substring: Brute force approach

--- SlidingWindow_TwoPointer/MinWindow.java
+++ SlidingWindow_TwoPointer/MinWindow.java
@@ -12,9 +12,38 @@ public class MinWindow {
         System.out.println("Minimum length substring containing all characters from \"" + t + "\" is: " + ans);
     }
 
+    // Brute force approach
     public static String minWindow(String s, String t) {
-        
+        int n = s.length();
+        int startindex = -1;
+        int minlength = Integer.MAX_VALUE;
         String output = "";
+
+
+        for(int i=0;i<n;i++){
+            int[] hash = new int[256];
+            for(int j=0;j<t.length();j++){
+                hash[t.charAt(j)]++;
+            }
+            int count = 0;
+
+            for(int j=i;j<n;j++){
+                if(hash[s.charAt(j)]>0){
+                    count++;
+                }
+                hash[s.charAt(j)]--;
+
+                if(count==t.length()){
+                    if(j-i+1<minlength){
+                        minlength = j-i+1;
+                        startindex = i;
+                    }
+                break;
+                }
+            }
+        }
+        if(startindex!=-1)
+            output = s.substring(startindex,startindex+minlength);
         
         return output;
     }
