8ee78da500d220184a4e91e9da1f72f749f20967
shivrya <shivrajkhose4@gmail.com>
2025-06-04 19:14:45 -0700

    Minimum Window Substring: Main method

--- SlidingWindow_TwoPointer/MinWindow.java
+++ SlidingWindow_TwoPointer/MinWindow.java
@@ -2,5 +2,20 @@ package Data_Structures_Algorithms.SlidingWindow_TwoPointer;
 
 public class MinWindow {
 
-    
+    public static void main(String[] args) {
+        String s = "ddaaabbca";
+        String t = "abc";
+
+        String ans = minWindow(s, t);
+
+        // Print the result
+        System.out.println("Minimum length substring containing all characters from \"" + t + "\" is: " + ans);
+    }
+
+    public static String minWindow(String s, String t) {
+        
+        String output = "";
+        
+        return output;
+    }
 }
