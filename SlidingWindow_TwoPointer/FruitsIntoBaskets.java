e469ef165f09a683fe4929990437a36d8a80b27b
shivrya <shivrajkhose4@gmail.com>
2025-06-06 14:03:06 -0700

    Fruits Into Baskets: Brute Force Approach

--- SlidingWindow_TwoPointer/FruitsIntoBaskets.java
+++ SlidingWindow_TwoPointer/FruitsIntoBaskets.java
@@ -1,6 +1,6 @@
 package Data_Structures_Algorithms.SlidingWindow_TwoPointer;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class FruitsIntoBaskets {
 
@@ -13,15 +13,27 @@ public class FruitsIntoBaskets {
         System.out.println("Maximum fruits the basket can have: " + length);
     }
 
+    // BRUTE FORCE
     public static int totalFruits(int[] fruits) {
         int maxlength = 0;
+        int n = fruits.length;
 
-
-
+        for(int left=0;left<n;left++){
+                HashSet<Integer> set = new HashSet<>();
+    
+                for(int right=left;right<n;right++){
+    
+                    set.add(fruits[right]);
+    
+                    if(set.size()>2){
+                            break;
+                    }
+                    if(set.size()==2)
+                        maxlength = Math.max(maxlength,right-left+1);
+                }
+            }
         return maxlength;
     }
-
-    
 
     
 }
