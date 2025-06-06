af6d1d3b22a2c15a270d004abfb6d5f74d151c76
shivrya <shivrajkhose4@gmail.com>
2025-06-06 14:01:27 -0700

    Fruits Into Baskets

---
+++ SlidingWindow_TwoPointer/FruitsIntoBaskets.java
@@ -0,0 +1,27 @@
+package Data_Structures_Algorithms.SlidingWindow_TwoPointer;
+
+import java.util.HashMap;
+
+public class FruitsIntoBaskets {
+
+    public static void main(String[] args) {
+        int[] input = {1,2,3,2,2};  
+        
+        int length = totalFruits(input);
+        
+        // Print the result
+        System.out.println("Maximum fruits the basket can have: " + length);
+    }
+
+    public static int totalFruits(int[] fruits) {
+        int maxlength = 0;
+
+
+
+        return maxlength;
+    }
+
+    
+
+    
+}
