af687f74b7b705bb3365c4519717fcc69d223ab3
shivrya <shivrajkhose4@gmail.com>
2025-06-08 23:10:00 -0700

    Implementation of Stack Using Queue

--- Stack_Queue/QueueStack.java
+++ Stack_Queue/QueueStack.java
@@ -1,5 +1,6 @@
 package Data_Structures_Algorithms.Stack_Queue;
 
+import java.lang.reflect.Method;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -40,6 +41,7 @@ public class QueueStack {
         
     }
 
+    // Method to push element in the stack
     public void push(int x) {
         // Get size
         int s = q.size();
@@ -52,15 +54,24 @@ public class QueueStack {
         }
     }
 
+    // Method to pop element from stack
     public int pop() {
-        
-      
+        // Get front element
+        int n = q.peek();
+        // Remove front element
+        q.poll();
+        // Return removed element
+        return n;
     }
 
+    // Method to return the top of stack
     public int top() {
-        
+        // Return front element
+        return q.peek();
     }
 
+    // Method to check if the stack is empty
     public boolean isEmpty() {
+        return q.isEmpty();
     }
 }
