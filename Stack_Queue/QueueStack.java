800a3daec7d5306f4f8babfedfcffc0914a5410b
shivrya <shivrajkhose4@gmail.com>
2025-06-08 23:07:51 -0700

    Implementation of Stack Using Queue

--- Stack_Queue/QueueStack.java
+++ Stack_Queue/QueueStack.java
@@ -3,6 +3,51 @@ package Data_Structures_Algorithms.Stack_Queue;
 public class QueueStack {
 
     public static void main(String[] args) {
+        QueueStack st = new QueueStack();
         
+        // Array of commands
+        String[] commands = {"QueueStack", "push", "push", 
+                             "pop", "top", "isEmpty"};
+        int[][] inputs = {{}, {4}, {8}, {}, {}, {}};
+
+        for (int i = 0; i < commands.length; ++i) {
+            switch (commands[i]) {
+                case "push":
+                    st.push(inputs[i][0]);
+                    System.out.print("null ");
+                    break;
+                case "pop":
+                    System.out.print(st.pop() + " ");
+                    break;
+                case "top":
+                    System.out.print(st.top() + " ");
+                    break;
+                case "isEmpty":
+                    System.out.print(st.isEmpty() ? "true " : "false ");
+                    break;
+                case "QueueStack":
+                    System.out.print("null ");
+                    break;
+            }
+        }
+    }
+    public QueueStack() {
+        
+    }
+
+    public void push(int x) {
+       
+    }
+
+    public int pop() {
+        
+      
+    }
+
+    public int top() {
+        
+    }
+
+    public boolean isEmpty() {
     }
 }
