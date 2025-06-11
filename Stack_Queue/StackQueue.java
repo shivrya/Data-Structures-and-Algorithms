85e82ddd45b6994c3db5d8f740d84c13a4be3f52
shivrya <shivrajkhose4@gmail.com>
2025-06-10 21:42:18 -0700

    Queue using stack

---
+++ Stack_Queue/StackQueue.java
@@ -0,0 +1,16 @@
+package Data_Structures_Algorithms.Stack_Queue;
+
+public class StackQueue {
+
+    public static void main(String[] args) {
+        StackQueue q = new StackQueue();
+
+        // List of commands
+        String[] commands = {"StackQueue", "push", "push", 
+                             "pop", "peek", "isEmpty"};
+        // List of inputs
+        int[][] inputs = {{}, {4}, {8}, {}, {}, {}};
+
+    }
+    
+}
