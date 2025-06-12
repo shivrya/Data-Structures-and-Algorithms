bd684091dbb0c1d228242e3c0e24eb4cc0e9c8e3
shivrya <shivrajkhose4@gmail.com>
2025-06-11 22:15:54 -0700

    Implement Queue using linkedlist

--- Stack_Queue/LinkedListQueue.java
+++ Stack_Queue/LinkedListQueue.java
@@ -1,8 +1,55 @@
 package Data_Structures_Algorithms.Stack_Queue;
 
+import java.util.LinkedList;
+
 public class LinkedListQueue {
+    private LinkedList<Integer> list;
 
+    public static void main(String[] args) {
+        // Creating a queue
+        LinkedListQueue q = new LinkedListQueue();
 
-    
+        // Array of commands
+        String[] commands = {"LinkedListQueue", "push", "push", 
+                             "peek", "pop", "isEmpty"};
+        // Array of inputs
+        int[][] inputs = {{}, {3}, {7}, {}, {}, {}};
 
+        for (int i = 0; i < commands.length; ++i) {
+            if (commands[i].equals("push")) {
+                q.push(inputs[i][0]);
+                System.out.print("null ");
+            } else if (commands[i].equals("pop")) {
+                System.out.print(q.pop() + " ");
+            } else if (commands[i].equals("peek")) {
+                System.out.print(q.peek() + " ");
+            } else if (commands[i].equals("isEmpty")) {
+                System.out.print((q.isEmpty() ? "true" : "false") + " ");
+            } else if (commands[i].equals("LinkedListQueue")) {
+                System.out.print("null ");
+            }
+        }
+    }
+
+    public LinkedListQueue() {
+        list = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        list.add(x);
+    }
+
+    public int pop() {
+        return list.removeFirst();
+      
+    }
+
+    public int peek() {
+        return list.getFirst();
+        
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
 }
