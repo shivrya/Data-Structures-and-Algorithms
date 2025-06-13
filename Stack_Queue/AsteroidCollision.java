c9f26009b9476a72ef516ed7baa4f4fff3b06ad7
shivrya <shivrajkhose4@gmail.com>
2025-06-12 17:17:08 -0700

    Asteroid Collision

---
+++ Stack_Queue/AsteroidCollision.java
@@ -0,0 +1,18 @@
+package Data_Structures_Algorithms.Stack_Queue;
+
+public class AsteroidCollision {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, -10};
+        
+
+        AsteroidCollision sol = new AsteroidCollision(); 
+        
+        // int[] ans = sol.asteroidCollision(arr);
+        
+        // System.out.print("The state of asteroids after collisions is: ");
+        // for(int i = 0; i < ans.length; i++) {
+        //     System.out.print(ans[i] + " ");
+        // }
+    }
+}
