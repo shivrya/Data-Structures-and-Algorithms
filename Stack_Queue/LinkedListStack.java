69ca621c6039a6c679f9f8eac543a36ca6ff7e62
shivrya <shivrajkhose4@gmail.com>
2025-06-13 00:44:13 -0700

    Stack Using Linked List

---
+++ Stack_Queue/LinkedListStack.java
@@ -0,0 +1,18 @@
+package Data_Structures_Algorithms.Stack_Queue;
+
+public class LinkedListStack {
+
+    public static void main(String[] args) {
+        // Creating a stack
+        LinkedListStack st = new LinkedListStack();
+
+        // Array of commands
+        String[] commands = {"LinkedListStack", "push", "push", 
+                             "pop", "top", "isEmpty"};
+        // Array of inputs
+        int[][] inputs = {{}, {3}, {7}, {}, {}, {}};
+
+        
+    }
+
+}
