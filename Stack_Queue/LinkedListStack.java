be4f6f0fd215b006df2ddcc6af354d023a7b27b6
shivrya <shivrajkhose4@gmail.com>
2025-06-13 00:46:09 -0700

    Stack Using Linked List

--- Stack_Queue/LinkedListStack.java
+++ Stack_Queue/LinkedListStack.java
@@ -1,6 +1,10 @@
 package Data_Structures_Algorithms.Stack_Queue;
 
+import java.util.LinkedList;
+
 public class LinkedListStack {
+
+    private LinkedList<Integer> list;
 
     public static void main(String[] args) {
         // Creating a stack
@@ -12,7 +16,41 @@ public class LinkedListStack {
         // Array of inputs
         int[][] inputs = {{}, {3}, {7}, {}, {}, {}};
 
+        for (int i = 0; i < commands.length; ++i) {
+            if (commands[i].equals("push")) {
+                st.push(inputs[i][0]);
+                System.out.print("null ");
+            } else if (commands[i].equals("pop")) {
+                System.out.print(st.pop() + " ");
+            } else if (commands[i].equals("top")) {
+                System.out.print(st.top() + " ");
+            } else if (commands[i].equals("isEmpty")) {
+                System.out.print((st.isEmpty() ? "true" : "false") + " ");
+            } else if (commands[i].equals("LinkedListStack")) {
+                System.out.print("null ");
+            }
+        }
+    }
+
+    public LinkedListStack() {
+        list = new LinkedList<>();
+    }
+
+    public void push(int x) {
+       list.add(x);
+    }
+
+    public int pop() {
+        return list.removeLast();
+      
+    }
+
+    public int top() {
+        return list.getLast();
         
     }
 
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
 }
