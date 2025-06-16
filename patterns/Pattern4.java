0d430bfc99cd5fd8da2428962ee32159f19c1fab
shivrya <shivrajkhose4@gmail.com>
2025-06-15 22:50:12 -0700

    Pattern 4 Implementation

--- patterns/Pattern4.java
+++ patterns/Pattern4.java
@@ -9,6 +9,7 @@ public class Pattern4 {
     // 55555
 
     public static void main(String[] args) {
+
         int N = 5;
 
         pattern4(N);
@@ -16,6 +17,15 @@ public class Pattern4 {
     }
 
     public static void pattern4(int n){
+
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println(" ");
+
+        }
 
     }
 
