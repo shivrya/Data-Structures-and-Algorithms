67346d49d3003faa9df313f24095afac16535664
shivrya <shivrajkhose4@gmail.com>
2025-06-15 21:09:17 -0700

    Pattern 4

---
+++ patterns/Pattern4.java
@@ -0,0 +1,17 @@
+package Data_Structures_Algorithms.Patterns;
+
+public class Pattern4 {
+
+    // 1
+    // 22
+    // 333
+    // 4444
+    // 55555
+
+    public static void main(String[] args) {
+        int N = 5;
+
+       
+    }
+
+}
