05ba177c8f9368be4334d58a05a375d3982e38b9
shivrya <shivrajkhose4@gmail.com>
2025-06-17 16:52:16 -0700

    Stack Queue: Sum of minimum values all subarray

--- Stack_Queue/SumOfSubarrayMinimums.java
+++ Stack_Queue/SumOfSubarrayMinimums.java
@@ -2,4 +2,25 @@ package Data_Structures_Algorithms.Stack_Queue;
 
 public class SumOfSubarrayMinimums {
 
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 2, 5};
+        
+        /* Creating an instance of 
+        Solution class */
+        SumOfSubarrayMinimums sol = new SumOfSubarrayMinimums(); 
+        
+        /* Function call to find the sum of the 
+        minimum value in each subarray */
+        int ans = sol.sumSubarrayMins(arr);
+        
+        System.out.println("The sum of minimum value in each subarray is: " + ans);
+    }
+
+    public int sumSubarrayMins(int[] arr){
+        int sum = 0;
+
+
+        return sum;
+    }
+
 }
