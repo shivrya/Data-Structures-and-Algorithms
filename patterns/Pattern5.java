a437e800ee42dad6535401348ca759dd189da7c3
shivrya <shivrajkhose4@gmail.com>
2025-06-16 23:11:25 -0700

    Pattern 5 implementation

--- patterns/Pattern5.java
+++ patterns/Pattern5.java
@@ -1,7 +1,24 @@
 package Data_Structures_Algorithms.Patterns;
 
+
+// *****
+// ****
+// ***
+// **
+// *
+
 public class Pattern5 {
 
+    public static void main(String[] args){
+        int N = 5;
+
+        pattern5(N);
+
+    }
+
+    public static void pattern5(int n){
+
+    }
     
 
 }
