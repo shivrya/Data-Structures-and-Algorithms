619706be2a9561ae994cf9853828b8b8d17bdd9e
shivrya <shivrajkhose4@gmail.com>
2025-06-16 23:14:15 -0700

    Pattern 5 Implementation

--- patterns/Pattern5.java
+++ patterns/Pattern5.java
@@ -18,6 +18,14 @@ public class Pattern5 {
 
     public static void pattern5(int n){
 
+        for(int i=n;i>0;i--){
+
+            for(int j=i;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
     }
     
 
