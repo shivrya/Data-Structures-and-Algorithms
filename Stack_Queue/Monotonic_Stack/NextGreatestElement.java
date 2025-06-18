160719b32295483475ae6a7ea8e4ac660b7f90ed
shivrya <shivrajkhose4@gmail.com>
2025-06-18 08:43:25 -0700

    Next Greatest Element: Brute Force Implementation

--- Stack_Queue/Monotonic_Stack/NextGreatestElement.java
+++ Stack_Queue/Monotonic_Stack/NextGreatestElement.java
@@ -1,5 +1,7 @@
 package Data_Structures_Algorithms.Stack_Queue.Monotonic_Stack;
 
+import java.util.Stack;
+
 public class NextGreatestElement {
     public static void main(String[] args) {
         int n = 4;
@@ -11,7 +13,7 @@ public class NextGreatestElement {
         
         /* Function call to find the next greater element
         for each element in the array */
-        int[] ans = sol.nextLargerElement(arr);
+        int[] ans = sol.nextLargerElement1(arr);
         
         System.out.println("The next greater elements are: ");
         for(int i = 0; i < n; i++) {
@@ -37,4 +39,26 @@ public class NextGreatestElement {
         return result;
     }
 
+    // Optimal Approach Using Stack
+    public int[] nextLargerElement1(int[] arr){
+        int n = arr.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[n];
+
+        for(int i=n-1;i>=0;i--){
+
+            while(!stack.isEmpty() && stack.peek() <= arr[i]){
+                stack.pop();
+            }
+
+            if(stack.isEmpty()){
+                result[i] = -1;
+                
+            }else{
+                result[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        return result;
+    }
 }
