e049e5e6403c4af22ec94c98b09d18c9707f3c23
shivrya <shivrajkhose4@gmail.com>
2025-06-19 02:57:43 -0700

    Trapping Rainwater main method

--- Stack_Queue/FAQ/Trapping_Rainwater.java
+++ Stack_Queue/FAQ/Trapping_Rainwater.java
@@ -2,4 +2,22 @@ package Data_Structures_Algorithms.Stack_Queue.FAQ;
 
 public class Trapping_Rainwater {
 
+    public static void main(String[] args) {
+        int[] heights = {4, 2, 0, 3, 2, 5};
+        
+        // Creating an instance of Solution class
+        Trapping_Rainwater sol = new Trapping_Rainwater();
+        
+        // Function call to get the trapped water
+        int ans = sol.trap(heights);
+        
+        System.out.println("The trapped rainwater is: " + ans);
+    }
+
+    public int trap(int[] height) {
+        int total = 0;
+
+        return total;
+    }
+
 }
