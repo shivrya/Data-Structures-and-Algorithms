d068985863957ced52ca5f4bc0f8a686c22ebe93
shivrya <shivrajkhose4@gmail.com>
2025-06-22 13:24:27 -0700

    Recursion : Check if Number is prime or not

---
+++ Recursion/Basics/PrimeOrNot.java
@@ -0,0 +1,18 @@
+package Data_Structures_Algorithms.Recursion.Basics;
+
+public class PrimeOrNot {
+
+    public static void main(String[] args) {
+        PrimeOrNot solution = new PrimeOrNot();
+        int num = 7; 
+        boolean result = solution.checkPrime(num); 
+        System.out.println(result); 
+    }
+
+    public boolean checkPrime(int num) {
+        return true;
+    }
+
+
+
+}
