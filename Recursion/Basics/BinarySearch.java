1caf3132ffed55d0c5d11bf3b7d0c8c9ce0389cc
shivrya <shivrajkhose4@gmail.com>
2025-06-23 23:59:39 -0700

    Recursion : Check if Number is prime or not

---
+++ Recursion/Basics/BinarySearch.java
@@ -0,0 +1,37 @@
+package Data_Structures_Algorithms.Recursion.Basics;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        BinarySearch b = new BinarySearch();
+
+        int[] arr = {1,2,3,4,5,6};
+
+        boolean result = b.BinarySearch(arr,4);
+
+        System.out.println(result);
+    }
+
+    public boolean BinarySearch(int[] arr,int element){
+
+        return binary(arr,element, 0, arr.length-1);
+
+    }
+
+    public boolean binary(int[] arr,int element, int low, int high){
+
+        if(low>=high)
+            return false;
+        
+        int mid = (low+high)/2;
+
+        if(arr[mid]==element)
+            return true;
+        else if(arr[mid]>element)
+            high = mid-1;
+        else
+            low = mid+1;
+
+        return binary(arr, element, low, high);
+    }
+}
---
+++ Recursion/Basics/Test.java
@@ -0,0 +1,12 @@
+package Data_Structures_Algorithms.Recursion.Basics;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int abc = 0;
+
+        System.out.println(++abc);
+        System.out.println(abc);
+    }
+
+}
