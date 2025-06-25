f89a33a9f6f5e25ae07df8ad44b35e8c4d298d7f
shivrya <shivrajkhose4@gmail.com>
2025-06-25 14:26:54 -0700

    Power of a number using recursion

---
+++ Recursion/Advanced/Power.java
@@ -0,0 +1,25 @@
+package Data_Structures_Algorithms.Recursion.Advanced;
+
+public class Power {
+
+    public static void main(String[] args) {
+        Power sol = new Power();
+        double x = 2.0;
+        int n = 10;
+
+        // Calculate x raised to n
+        double result = sol.myPow(x, n);
+
+        // Print the result
+        System.out.println(x + "^" + n + " = " + result);
+    }
+
+    public double myPow(double x, int n){
+        if(n==0)
+            return 1.0;
+        
+
+        return 1.0;
+    }
+
+}
