d909d77111f147c6cbd8236b8f6ad7f90656cd11
shivrya <shivrajkhose4@gmail.com>
2025-07-15 22:06:14 -0700

    Binary Tree Level Order

---
+++ BinaryTrees/LevelOrder.java
@@ -0,0 +1,31 @@
+package Data_Structures_Algorithms.BinaryTrees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LevelOrder {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        LevelOrder solution = new LevelOrder();
+        List<List<Integer>> result = solution.verticalTraversal(root);
+
+        System.out.println("Vertical Traversal:");
+        for (List column : result) {
+            System.out.println(column);
+        }
+    }
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        return result;
+    }
+
+}
