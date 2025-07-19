9b6543d8dcdf662b6a6fadb1bc8e4cea9a1b8514
shivrya <shivrajkhose4@gmail.com>
2025-07-19 03:58:29 -0700

    Level Order Traversal of Binary Tree

--- BinaryTrees/LevelOrder.java
+++ BinaryTrees/LevelOrder.java
@@ -1,7 +1,11 @@
 package Data_Structures_Algorithms.BinaryTrees;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.TreeMap;
 
 class TreeNode {
     int data;
@@ -48,8 +52,36 @@ public class LevelOrder {
     }
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        Queue<Tuple> queue = new LinkedList<>();
+        queue.offer(new Tuple(root,0,0));
 
+        while(!queue.isEmpty()){
+            Tuple current = queue.poll();
+            TreeNode node = current.node;
+            int col = current.col;
+            int row = current.row;
+
+            map.putIfAbsent(col, new TreeMap<>());
+            map.get(col).putIfAbsent(row, new PriorityQueue<>());
+            map.get(col).get(row).offer(node.data);
+
+            if(node.left!=null)
+                queue.offer(new Tuple(node.left,col-1,row+1));
+            if(node.right!=null)
+                queue.offer(new Tuple(node.right,col+1,row+1));
+        }
+
+        for(TreeMap<Integer, PriorityQueue<Integer>> ymap : map.values()){
+            List<Integer> answer = new ArrayList<>();
+
+            for(PriorityQueue<Integer> n : ymap.values()){
+                while(!n.isEmpty()){
+                    answer.add(n.poll());
+                }
+            }
+            result.add(answer);
+        }
         return result;
     }
-
 }
