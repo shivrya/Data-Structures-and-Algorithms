b3893c80d106e46a476c224fa9581635cded2d7c
shivrya <shivrajkhose4@gmail.com>
2025-07-25 16:26:37 -0700

    Refactoring changes

---
+++ Binary_Search_Tree/Basics/BSTSearch.java
@@ -0,0 +1,42 @@
+package Data_Structures_Algorithms.Binary_Search_Tree.Basics;
+
+class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val) { data = val; left = null; right = null; }
+}
+
+
+public class BSTSearch {
+
+    public static void main(String[] args) {
+        // Creating a simple BST for demonstration
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+
+        BSTSearch sol = new BSTSearch();
+        TreeNode result = sol.searchBST(root, 2);
+        if (result != null) {
+            System.out.println("Node found with value: " + result.data);
+        } else {
+            System.out.println("Node not found");
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val){
+        if(root == null)
+            return null;
+        
+        if(root.data == val)
+            return root;
+        else if(root.data > val){
+            return searchBST(root.left,val);
+        }
+        return searchBST(root.right, val);
+    }
+
+}
---
+++ Binary_Search_Tree/Basics/FloorCeil.java
@@ -0,0 +1,47 @@
+package Data_Structures_Algorithms.Binary_Search_Tree.Basics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FloorCeil {
+    public static void main(String[] args) {
+        // Creating a sample BST
+        TreeNode root = new TreeNode(8);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(12);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(6);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(14);
+
+        FloorCeil sol = new FloorCeil();
+        int key = 11;  // Key to find floor and ceil for
+
+        // Find and print floor and ceil values
+        List<Integer> result = sol.floorCeilOfBST(root, key);
+        System.out.println("Floor: " + result.get(0) + ", Ceil: " + result.get(1));
+    }
+
+    public List<Integer> floorCeilOfBST(TreeNode root, int key) {
+            List<Integer> result = new ArrayList<>();
+            int floor = -1;
+            int ceil = -1;
+
+            while(root!=null){
+                if(root.data==key){
+                    floor = root.data;
+                    ceil = root.data;
+                    break;
+                }else if(root.data > key){
+                    ceil = root.data;
+                    root = root.left;
+                }else{
+                    floor = root.data;
+                    root = root.right;
+                }
+            }
+            result.add(floor);
+            result.add(ceil);
+            return result;
+    }
+}
