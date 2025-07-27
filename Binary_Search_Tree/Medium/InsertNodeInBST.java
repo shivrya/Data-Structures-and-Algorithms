caaa3bb5ab59278c5370fcfc3f1d61556e7c3b3e
shivrya <shivrajkhose4@gmail.com>
2025-07-26 23:39:07 -0700

    Insert Node in BST

---
+++ Binary_Search_Tree/Medium/InsertNodeInBST.java
@@ -0,0 +1,28 @@
+package Data_Structures_Algorithms.Binary_Search_Tree.Medium;
+
+class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) { data = val; left = null; right = null; }
+ }
+
+public class InsertNodeInBST {
+
+    public static void main(String[] args) {
+        InsertNodeInBST solution = new InsertNodeInBST();
+        
+        // Create a sample BST: [4, 2, 7, 1, 3]
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        
+        int val = 5;
+        // root = solution.insertIntoBST(root, val);
+        
+        // Print the BST in-order to verify the insertion
+        // solution.printInOrder(root); // Output should be: 1 2 3 4 5 7
+    }
+}
