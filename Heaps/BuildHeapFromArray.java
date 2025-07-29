7d797d4f1e9c857845c813ad833161c5ba6eb9aa
shivrya <shivrajkhose4@gmail.com>
2025-07-28 23:52:03 -0700

    getting started with the heaps

--- Heaps/BuildHeapFromArray.java
+++ Heaps/BuildHeapFromArray.java
@@ -25,7 +25,24 @@ public class BuildHeapFromArray {
         for(int i=nums.length-1;i>=0;i--){
         //    heapifyDown(nums,i);
         }
-
     }
 
+    public void heapifyDown(int[] nums, int ind){
+        int leftchild = ind * 2 + 1;
+        int rightchild = ind * 2 + 2;
+        int smallest = ind;
+
+        if(leftchild < nums.length && nums[smallest] > nums[leftchild]){
+             smallest = leftchild;
+        }
+        if(rightchild < nums.length && nums[smallest] > nums[rightchild]){
+         smallest = rightchild;
+        }
+        if(smallest!=ind){
+         int tmp = nums[smallest];
+         nums[smallest] = nums[ind];
+         nums[ind] = tmp;
+         heapifyDown(nums,smallest);
+        }
+    }
 }
