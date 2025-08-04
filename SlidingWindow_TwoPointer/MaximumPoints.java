0a473f6610103bfa436c3a792dfa7863705ee27f
shivrya <shivrajkhose4@gmail.com>
2025-08-03 21:47:31 -0700

    Maximum points from k cards

---
+++ SlidingWindow_TwoPointer/MaximumPoints.java
@@ -0,0 +1,19 @@
+package Data_Structures_Algorithms.SlidingWindow_TwoPointer;
+
+public class MaximumPoints {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6};
+        
+        // Create an instance of the Solution class
+        MaximumPoints sol = new MaximumPoints();
+
+        // int result = sol.maxScore(nums, 3);
+
+        // Output the maximum score
+        System.out.println("The maximum score is:");
+        // System.out.println(result);
+    }
+
+    
+}
